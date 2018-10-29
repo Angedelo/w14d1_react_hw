06a31d5cc09af24471714c8ba24c2e3b9d53cd19
Matt Addison <matthew.addison@talk21.com>
2018-10-29 17:19:51 +0000

    adds props to filmreleases and link to more

--- src/containers/FilmReleases.js
+++ src/containers/FilmReleases.js
@@ -2,14 +2,50 @@ import React, { Component } from 'react';
 import FilmReleasesList from '../components/FilmReleasesList';
 
 class FilmReleases extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      data: [
+        {
+          id: 1,
+          name: "Spider-Man: Into the Spider-Verse",
+          url: "https://www.imdb.com/title/tt4633694/?ref_=rlm"
+        },
+        {
+          id: 2,
+          name: "Life Itself",
+          url: "https://www.imdb.com/title/tt5989218/?ref_=rlm"
+        },
+        {
+          id: 3,
+          name: "Mary Queen of Scots",
+          url: "https://www.imdb.com/title/tt2328900/?ref_=rlm"
+        },
+        {
+          id: 4,
+          name: "The Lego Movie 2: The Second Part", url: "https://www.imdb.com/title/tt3513498/?ref_=rlm"
+        },
+        {
+          id: 5,
+          name: "Captain Marvel",
+          url: "https://www.imdb.com/title/tt4154664/?ref_=rlm"
+        }
+      ]
+    }
+
+  }
+
   render(){
     return(
       <div className="film-releases">
         <h2>Upcoming Film Releases</h2>
-        <FilmReleasesList />
+        <FilmReleasesList data={this.state.data} />
+        <p>Click <a href="https://www.imdb.com/calendar/?region=gb">here</a> to see more upcoming releases</p>
       </div>
     )
   }
+
 }
 
 export default FilmReleases;
